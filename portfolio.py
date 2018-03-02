daa1397cdcdc38fe1850b4ffe164ea319dae1b2a
nmchenry01 <nmchenry@live.unc.edu>
2018-03-01 19:37:18 -0500

    First working version

--- portfolio.py
+++ portfolio.py
@@ -1,23 +1,26 @@
 import numpy as np
 import pandas as pd
+import scipy.optimize
 
 class portfolio(object):
     
     #Default constructor
-    def __init__(self, return_dict, interest_rates, prestart_return_dict, frequency): #frequency can be daily, weekly, monthly
+    def __init__(self, start_date, end_date, return_dict, interest_rates, prestart_return_dict, frequency): #frequency can be daily, weekly, monthly
         self.tickers = return_dict.keys() #Stock symbols are the keys 
+        self.start_date = start_date
+        self.end_date = end_date
         self.return_dict = return_dict
         self.interest_rates = interest_rates
-        self.portfolio = None
-        self.weights = np.ones(len(self.tickers)) #Initialize as a array of ones
+        self.assets = None
+        self.initial_weights = np.ones(len(self.tickers)) / len(self.tickers) #Initialize to equal weight
+        self.optimized_weights = {} #see optimize portfolio
         self.prestart_return_dict = prestart_return_dict #Used to calculate the first covariance value
         self.dates = return_dict[next(iter(return_dict))].index #Finds an arbitrary key, gets the datetime index from it
-        self.all_dates = return_dict[next(iter(self.return_dict))].resample('D').sum() #Includes NAN dates
         self.frequency = frequency
         self.rebalance_dates = None
-        self.rebalance_date_returns = None #empty for now, will be appended to later
+        self.rebalance_date_returns = {} #see optimize portfolio
         self.calculate_rebalance_date() #Calculate the rebalance dates depending on frequency
-        self.calculate_portfolio() #Forms n * m matrix 
+        self.calculate_assets() #Forms n * m matrix 
 
     #Fill in rebalance dates
     def calculate_rebalance_date(self):
@@ -27,49 +30,79 @@ class portfolio(object):
             temp = self.return_dict[next(iter(self.return_dict))].resample('W').sum()
             self.rebalance_dates = temp.index
         elif self.frequency == 'monthly': #Handle monthly case
-            temp = self.return_dict[next(iter(self.return_dict))].resample('M').sum()
-            self.rebalance_dates = temp.index
+            temp = self.return_dict[next(iter(self.return_dict))].resample('M').sum().index[:-1]
+            date_list = [self.return_dict[next(iter(self.return_dict))].index[0]]
+            for t in temp:
+                date_list.append(t)
+            self.rebalance_dates = date_list
 
     #Build an n by m portfolio with log returns for all assets over date range
-    def calculate_portfolio(self):
-        #Compute n by m portfolio
+    def calculate_assets(self):
+        #Compute n by m asset matrix
         temp_dict = {}
-        index = self.return_dict[next(iter(self.return_dict))].index
         for key in self.return_dict.keys():
             temp_dict[key] = self.return_dict[key]['Log Returns']
     
-        self.portfolio = pd.DataFrame(temp_dict) 
+        self.assets = pd.DataFrame(temp_dict) 
             
     #Portfolio return (scalar)
-    def calculate_total_return(weights): 
-        returns_vector = self.portfolio.dot(weights)
+    def calculate_total_return(self, weights, assets): 
+        returns_vector = assets.dot(weights)
         return returns_vector.sum()
 
     #Portfolio return (vector)
-    def calculate_vector_return(): #matrix of assets (N [days] * M [assets]), vector of weightings (M*1)
+    def calculate_vector_return(self, weights, assets): 
+        return assets.dot(weights)
         
-        #Description
-        #We take the two matracies, and multiple them together ((N*M)*(M*1))
-        #This yields a vector (M*1) of daily returns for the portfolio
+    def sum_squared_differences(self, weights, assets):
+        portfolio_return = self.calculate_vector_return(weights, assets) 
+        portfolio_mean_return = portfolio_return.mean()
+        sum_squared_differences = ((portfolio_return-portfolio_mean_return)**2).sum()
 
-        #Output
-        #A vector representing the return for the portfolio for each day over the period
+        return sum_squared_differences
 
-    def calculate_st_dev(): #Weight vector (1*M), rebalancing frequency, cov-interval (4 weeks prior), return_dict (so that we can access log return for each asset) (ALREADY AVAILABLE VIA CONSTRUCTOR)
+    def sharpe_ratio(self, weights, assets): 
+        portfolio_return = self.calculate_vector_return(weights, assets)
+        portfolio_return_minus_rf = portfolio_return - self.interest_rates["daily_rf_rate"]
+        sharpe_ratio = portfolio_return_minus_rf.mean()/self.sum_squared_differences(weights, assets)
+
+        return sharpe_ratio
+
+    def objective_function(self, weights, assets):
+        return (-1 * self.sharpe_ratio(weights, assets))
+
+    def optimize_portfolio(self):
+        #Constraints
+        cons = {'type':'eq', 
+                'fun': lambda x: np.sum(np.abs(x)) - 1}  #Weights must sum to one (absolute value)
+        #Element bounds
+        bounds = [(-1, 1.5)] * len(self.initial_weights) #Shorts are allowed, leverage of up to 1.5 times
+        date_len = len(self.rebalance_dates)
+    
+        for i in range(date_len):
+            if date_len - i == 1:
+                #This is the last value
+                sliced_assets = self.assets.loc[self.rebalance_dates[i] : self.end_date]
+            else:
+                #Either the first or an inner value
+                sliced_assets = self.assets.loc[self.rebalance_dates[i] : self.rebalance_dates[i+1]]
+                
+            
+            results = scipy.optimize.minimize(self.objective_function, self.initial_weights, args=(sliced_assets), 
+                                                method= "SLSQP", constraints=cons, bounds=bounds, 
+                                                options={'disp': True, 
+                                                         'maxiter': 1000})
+            
+            #Assign weights
+            self.optimized_weights[self.rebalance_dates[i].strftime('%Y-%m-%d')] = results.x
+            
+            #Assign return values
+            self.rebalance_date_returns[self.rebalance_dates[i].strftime('%Y-%m-%d')] = float(self.calculate_total_return(results.x, sliced_assets))
         
-        #Description
-        #w'Sw
-        #Multiply the weight vector transpose by the S matrix and then again by the weight vector
-        #See picture for outline of S
-        #Take square root of resulting scalar
-
-        #Output
-        #Scalar representation of standard deviation
-
-    def calculate_sharpe_ratio(weights): #Takes in a vector of weights 
+        return {"optimized_weights":self.optimized_weights,
+                "optimized_returns": self.rebalance_date_returns
+                }
 
 
 
-    def optimize_portfolio():
-        #INSERT CODE
 
