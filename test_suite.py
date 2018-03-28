d49a91742784a891a8acb6a7db77d04369c50d01
nmchenry01 <nmchenry@live.unc.edu>
2018-03-28 09:43:08 -0400

    Added a test for calculate return for stock dict

--- test_suite.py
+++ test_suite.py
@@ -3,6 +3,8 @@ from data import pull_data, get_risk_free_rate, calculate_returns
 from portfolio import Portfolio
 import pandas as pd
 
+#Think about using fixtures to setup configuration
+
 class TestDataClass(object):
     #Test to make sure we get same number of assets back from datapull
     def test_stock_count(self):
@@ -38,3 +40,15 @@ class TestDataClass(object):
         interest_rates = get_risk_free_rate(start_date,end_date)
 
         assert abs(interest_rates['daily_rf_rate'] - .0041) < .0001  #Known to be approximately .0041 for the month of January 2018
+
+    #Test to ensure that we are calculating log returns correctly
+    def test_calculate_return(self):
+        #Configuration
+        price_list = [10,11]
+        test_df = pd.DataFrame(price_list,columns=['Adj_Close']) #Test values
+        test_dict = {'FB':test_df}
+
+        #Nested JSON
+        results = calculate_returns(test_dict)
+
+        assert (results['FB']['Log Returns'].iloc[0] - .0953) < .0001 #Log percent change between 10 and 11 is approx 9.53%
